47c19a432c67ef6075597fa83bcec82c6ba7f971
Nils <n.matthiessen@stud.uni-goettingen.de>
2026-06-23 00:45:09 +0200

    Added LinReg

--- src/models/LinReg.py
+++ src/models/LinReg.py
@@ -1,47 +1,30 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "74062aa2",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "class LinearRegression2(torch.nn.Module):\n",
-    "    \"\"\"\n",
-    "    Linear regression model inherits the torch.nn.Module\n",
-    "    which is the base class for all PyTorch modules.\n",
-    "    \"\"\"\n",
-    "    def __init__(self, input_dim, output_dim):\n",
-    "        \"\"\" Initializes internal Module state. \"\"\"\n",
-    "        super(LinearRegression, self).__init__()\n",
-    "        # TODO: define a linear layer for the model using torch.nn.Linear\n",
-    "        self.linear = torch.nn.Linear(input_dim, output_dim)\n",
-    "\n",
-    "\n",
-    "    def forward(self, x):\n",
-    "        \"\"\" Defines the computation performed at every call. \"\"\"\n",
-    "        # TODO: flatten the input to a suitable size for the linear layer \n",
-    "        # Hint: use x.reshape(...) and the input_dim variable\n",
-    "        \n",
-    "        # Deine Zeile:\n",
-    "        # x = x.reshape(x.shape[0], -1)\n",
-    "        \n",
-    "        # Bessere Alternative (Standard in PyTorch):\n",
-    "        # -1 bedeutet automatisch: \"Rechne die zweite Dimension aus\"\n",
-    "        x = x.view(x.size(0), -1)\n",
-    "        \n",
-    "        # TODO: run the flattened data through the linear layer and return the outputs\n",
-    "        outputs = self.linear(x)\n",
-    "        return outputs"
-   ]
-  }
- ],
- "metadata": {
-  "language_info": {
-   "name": "python"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import torch
+import torch.nn as nn
+
+class LinearRegression2(torch.nn.Module):
+    """
+    Linear regression model inherits the torch.nn.Module
+    which is the base class for all PyTorch modules.
+    """
+    def __init__(self, input_dim, output_dim):
+        """ Initializes internal Module state. """
+        super(LinearRegression, self).__init__()
+        # TODO: define a linear layer for the model using torch.nn.Linear
+        self.linear = torch.nn.Linear(input_dim, output_dim)
+
+
+    def forward(self, x):
+        """ Defines the computation performed at every call. """
+        # TODO: flatten the input to a suitable size for the linear layer 
+        # Hint: use x.reshape(...) and the input_dim variable
+        
+        # Deine Zeile:
+        # x = x.reshape(x.shape[0], -1)
+        
+        # Bessere Alternative (Standard in PyTorch):
+        # -1 bedeutet automatisch: "Rechne die zweite Dimension aus"
+        x = x.view(x.size(0), -1)
+        
+        # TODO: run the flattened data through the linear layer and return the outputs
+        outputs = self.linear(x)
+        return outputs
